97ceffc03f6a8234317cc1e726e77056e8498a52
Thomas Slade <thomas@blueacorn.com>
2015-06-30 11:08:36 -0400

    PATCH SUPEE 2217

--- app/code/core/Mage/Catalog/Model/Resource/Product/Collection.php
+++ app/code/core/Mage/Catalog/Model/Resource/Product/Collection.php
@@ -909,6 +909,7 @@ class Mage_Catalog_Model_Resource_Product_Collection extends Mage_Catalog_Model_
         $countSelect->columns('COUNT(DISTINCT e.entity_id)');
         if ($resetLeftJoins) {
             $countSelect->resetJoinLeft();
+            $countSelect->reset(Zend_Db_Select::GROUP);
         }
         return $countSelect;
     }
